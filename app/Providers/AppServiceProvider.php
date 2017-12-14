611890abeb085e56d2cc59a72391492262786352
jsaza <jy.saza@gmail.com>
2017-12-14 07:04:58 +0000

    スキーマ文字列調整

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //スキーマの文字列の長さを190にセット
+        \Schema::defaultStringLength(191);
     }
 
     /**
