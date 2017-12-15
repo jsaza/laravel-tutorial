8ef8020eab891dd6d3284b849a0362b1e8f8ee80
jsaza <jy.saza@gmail.com>
2017-12-15 08:18:08 +0000

    日本語化対応 comja5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    あなたはログイン済みです！
                 </div>
             </div>
         </div>
