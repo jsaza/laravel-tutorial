38dbfc967b726d5f34b4b9fc4015e3831288f4f0
jsaza <jy.saza@gmail.com>
2017-12-27 10:04:10 +0000

    G2FA認証のQRを再表示/これは再登録？

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::get('/complete-registration', 'Auth\RegisterController@completeRegistrati
 Route::post('/2fa', function () {
     return redirect(URL()->previous());
 })->name('2fa')->middleware('2fa');
+Route::get('/re-authenticate', 'HomeController@reauthenticate');
