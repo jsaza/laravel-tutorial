9a6f7f7a26d9dbe27ed58a289815a6c515525951
jsaza <jy.saza@gmail.com>
2017-12-28 06:51:56 +0000

    Social連携設定ファイル

--- config/services.php
+++ config/services.php
@@ -34,5 +34,31 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
-
+    
+    //Socialite
+    'facebook' => [
+            'client_id'     => env('FB_CLIENT_ID'),
+            'client_secret' => env('FB_CLIENT_SECRET'),
+            'redirect'      => env('FB_URL'),
+    ],
+    'twitter' => [
+            'client_id'     => env('TWITTER_CLIENT_ID'),
+            'client_secret' => env('TWITTER_CLIENT_SECRET'),
+            'redirect'      => env('TWITTER_URL'),
+    ],
+    'google' => [
+            'client_id'     => env('GOOGLE_CLIENT_ID'),
+            'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+            'redirect'      => env('GOOGLE_URL'),
+    ],
+    'github' => [
+            'client_id'     => env('GITHUB_CLIENT_ID'),
+            'client_secret' => env('GITHUB_CLIENT_SECRET'),
+            'redirect'      => env('GITHUB_URL'),
+    ],
+    'bitbucket' => [
+            'client_id'     => env('BITBUCKET_CLIENT_ID'),
+            'client_secret' => env('BITBUCKET_CLIENT_SECRET'),
+            'redirect'      => env('BITBUCKET_URL'),
+    ],
 ];
