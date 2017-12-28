5b93e8f41906f3a91eda849f728c35e512829f11
jsaza <jy.saza@gmail.com>
2017-12-28 06:51:25 +0000

    Social連携モデル

--- app/LinkedSocialAccount.php
+++ app/LinkedSocialAccount.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LinkedSocialAccount extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['provider_name', 'provider_id' ];
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
