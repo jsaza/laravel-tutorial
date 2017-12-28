235f3d7cee359450fe017d0abe36a6aa55e018f0
jsaza <jy.saza@gmail.com>
2017-12-28 06:48:13 +0000

    Social連携時のURL設定

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Socialite
+Route::get('login/{provider}/callback', 'Auth\SocialAccountController@handleProviderCallback');
+Route::get('login/{provider}',          'Auth\SocialAccountController@redirectToProvider');
